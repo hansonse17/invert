39b53e0e0690400b02eff0aa95e9d83a0cfc6cac
hansonse17 <hansonse17@grinnell.edu>
2017-04-28 09:53:34 -0500

    progress

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 void printdir(char* path) {
   DIR *dp = opendir(path); 
@@ -14,11 +15,17 @@ void printdir(char* path) {
     while (ep = readdir(dp)) {
       struct stat buf;
       char * name = ep->d_name;
-      char * path_name = path;
-      strncat(path_name, name, sizeof(name));
+      char * path_name = malloc(strlen(path)+strlen(name)+1);
+      strcpy(path_name, path);
+      strcat(path_name, "/");
+      strcat(path_name, name);
       stat((const char*)path_name, &buf);
       printf("%s %d\n", path_name, S_ISDIR(buf.st_mode));
-//      puts(ep->d_name);
+      if((S_ISDIR(buf.st_mode) == 1) && 
+         (strcmp(name, ".")) && 
+         (strcmp(name, ".."))) { //if current directory entry is another direcory
+        printdir(path_name);
+      }
     }
   (void) closedir (dp);
   }
@@ -27,18 +34,45 @@ void printdir(char* path) {
   }
 }
 
+void all_cap (char * string) {
+  char tmp[3] = "st";//(char )string;
+  char* ret = malloc(strlen(string) + 1);
+  int i = 0;
+  while(tmp[i]) {
+      putchar( toupper(tmp[i]));
+    //  strcat(ret, (char)toupper(tmp[i]));
+      i++;
+  }
+  printf("%s\n", ret);
+
+  return;
+} 
+
+void sendToBucket (char* word, char* path) {
+  char * bucket_dir = malloc(strlen("./buckets") + strlen(path) + 1);
+  strcpy(bucket_dir, "./buckets");
+  
+  char * hash = malloc(sizeof(char) * 3);
+  memcpy(hash, word, 2);
+  strcat(hash, "\0");
+  printf("Hash : %s\n", hash);
+  
+  all_cap(hash);
+  
+  return;
+
+}
+
+
 int main (void) {
 
-  char* path = ("./");
+  char* path = ("./sample");
   
-  printdir(path);
+ // printdir(path);
+
+  sendToBucket("hello", "./world.txt");
 
 
- // struct stat buf;
-
- // stat(path, &buf);
-
- // printf("%d\n",S_ISDIR(buf.st_mode));//0 is false
   
   
   return 0;
